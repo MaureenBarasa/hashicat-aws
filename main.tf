5c1eb317f3237c574f973f47040022dedf3293e7
maureen barasa <maureen.barasa.mb@gmail.com>
2023-09-18 15:57:05 +0000

    modified main.tf

--- main.tf
+++ main.tf
@@ -197,5 +197,5 @@ locals {
 
 resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
-  public_key = tls_private_key.hashicat.public_key_openssh
+  public_key = tls_private_key.hashicat2.public_key_openssh
 }
